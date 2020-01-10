767c36ef8172efdd0b4f372b7bc8ea35d0e1a244
kuro <dirkreher@ymail.com>
2020-01-10 14:20:41 +0100

    added basic gui implementation

--- src/main/java/mastermind/game/gui/WindowController.java
+++ src/main/java/mastermind/game/gui/WindowController.java
@@ -1,4 +1,18 @@
 package mastermind.game.gui;
 
-public class WindowController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WindowController implements Initializable {
+    @FXML
+    private HBox mainColourBox;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
